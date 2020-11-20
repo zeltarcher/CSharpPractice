f6729a46b91f30fc44fd590377e865e5aa2c585b
zeltarcher <truongchiquang@gmail.com>
2020-11-20 01:43:19 -0600

    change name classes

--- ConsoleApp1/ConsoleApp1/WarmupProblems/JumpingOnTheClouds.cs
+++ ConsoleApp1/ConsoleApp1/WarmupProblems/JumpingOnTheClouds.cs
@@ -12,51 +12,54 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class Solution
+namespace ConsoleApp1
 {
-
-    // Complete the jumpingOnClouds function below.
-    static int jumpingOnClouds(int[] c)
+    class JumpingOnTheCloud
     {
-        int jumps = 0;
-        int position = 2;//check start from position 2
-        while (position < c.Length)
+
+        // Complete the jumpingOnClouds function below.
+        static int jumpingOnClouds(int[] c)
         {
-            if (c[position] != 0)
+            int jumps = 0;
+            int position = 2;//check start from position 2
+            while (position < c.Length)
             {
-                position--;
+                if (c[position] != 0)
+                {
+                    position--;
+                }
+                jumps++;
+                position += 2;
             }
-            jumps++;
-            position += 2;
+            //because position == c.length will not add any jump in the counting, so fix this by below code
+            if (position == c.Length)
+                jumps++;
+            return jumps;
         }
-        //because position == c.length will not add any jump in the counting, so fix this by below code
-        if (position == c.Length)
-            jumps++;
-        return jumps;
-    }
-    // Better Approach: this is opposite we start with 1 jump only, if this is not 0, then it must be 2 jump and position is moved to i+2 before count
-    /*     
-      int count = 0;
-        for (int i = 0; i < c.Length - 1; i++) {
-            if (c[i] == 0) i++;
-            count++;
+        // Better Approach: this is opposite we start with 1 jump only, if this is not 0, then it must be 2 jump and position is moved to i+2 before count
+        /*     
+          int count = 0;
+            for (int i = 0; i < c.Length - 1; i++) {
+                if (c[i] == 0) i++;
+                count++;
+            }
+            return count;
+         */
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+            ;
+            int result = jumpingOnClouds(c);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
-        return count;
-     */
-
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
-        ;
-        int result = jumpingOnClouds(c);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
     }
 }
