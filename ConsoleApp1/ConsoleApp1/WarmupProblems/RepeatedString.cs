cbde4e9ae54078495599d6f1e1554a56d7cc0904
zeltarcher <truongchiquang@gmail.com>
2020-11-30 03:14:10 -0600

    Update RepeatedString.cs

--- ConsoleApp1/ConsoleApp1/WarmupProblems/RepeatedString.cs
+++ ConsoleApp1/ConsoleApp1/WarmupProblems/RepeatedString.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
+using System;
 
-namespace ConsoleApp1
-{
-    class RepeatedString
-    {
+class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(string s, long n) {
+        long m=0,count=0,first_count=0,second_count=0;
+        for(int i = 0; i < s.Length; i++){
+            if(s[i]=='a'){
+                count++;
+            }
+        }
+        first_count=count*(n/s.Length);
+        for(int i=0;i<(n%s.Length);i++){
+            if(s[i]=='a'){
+                second_count++;
+            }
+        }
+        return first_count+second_count;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
