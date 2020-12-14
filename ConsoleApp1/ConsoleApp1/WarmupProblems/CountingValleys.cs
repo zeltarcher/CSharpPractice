e4462a0c328b2b6dd9506eae162d1c3aca2de8e4
zeltarcher <truongchiquang@gmail.com>
2020-12-13 22:09:03 -0600

    Create CountingValleys.cs

---
+++ ConsoleApp1/ConsoleApp1/WarmupProblems/CountingValleys.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int count = 0, lv = 0;//lv=0 is sea level
+        for (int i = 0; i < steps; i++)
+        {
+            if (path[i] == 'D')
+            {
+                lv--;
+            }
+            else if (path[i] == 'U')
+            {
+                lv++;
+                if (lv == 0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
